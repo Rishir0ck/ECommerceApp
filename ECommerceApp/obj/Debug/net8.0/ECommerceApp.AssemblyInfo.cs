6253f6b23f50407d9db3ae973980a4adcf26ad94
Rishi Prajapati <rhp1508@gmail.com>
2025-03-11 15:53:26 +0530

    fetch

--- ECommerceApp/obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
+++ ECommerceApp/obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e53f2bc9ea0c41d1441137e91db7f75c839bd62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc17f26bf8fd6585400717e179e69ad4a464e4a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
